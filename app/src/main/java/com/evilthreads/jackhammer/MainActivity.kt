132dd244199f80bd432f4b15631c6ab9a83132ad
evilthreads669966 <evilthreads669966@gmail.com>
2020-10-05 16:02:15 -0500

    add bootlaces to demo app.

--- app/src/main/java/com/evilthreads/jackhammer/MainActivity.kt
+++ app/src/main/java/com/evilthreads/jackhammer/MainActivity.kt
@@ -1,33 +1,21 @@
 package com.evilthreads.jackhammer
 
-import android.Manifest
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.candroid.bootlaces.bootService
 import com.evilthreads.jackhammerlib.checkOverlayPermission
-import com.evilthreads.jackhammerlib.requestpermission
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        bruteForce()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        bruteForce()
+        bootService(this){
+            service = MyService::class
+        }
     }
 
-    fun bruteForce(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            if(checkOverlayPermission())
-                requestpermission(Manifest.permission.READ_CALENDAR){
-                    Log.d("JACKJAMMER", "WE DID IT")
-                }
-        }
+    override fun onResume() {
+        super.onResume()
+        checkOverlayPermission()
     }
 }
